b79f59f5421ecdc578373bd2db83c1b3195c9000
bradborn13 <bradborn13@yahoo.com>
2026-02-19 13:13:06 +0100

    initial commit

--- backend/src/locations/dto/input/FindActivities.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-
-export class FindActivitiesDto {
-  @IsString()
-  @IsNotEmpty()
-  city: string;
-}
--- backend/src/locations/dto/output/ActivitiesDto.ts
+++
@@ -1,9 +0,0 @@
-import { Expose } from 'class-transformer';
-import { Activities } from 'src/activities/infrastructure/activities.schema';
-
-export class ActivitiesDto extends Activities {
-  @Expose()
-  get id() {
-    return this._id;
-  }
-}
